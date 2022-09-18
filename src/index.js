fe2dd8e7d9d72105336c6e8a0d4f702cab3db290
Shifts <74390871+ignshifts@users.noreply.github.com>
2022-09-18 18:26:43 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const { Client, MessageEmbed, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
+require('dotenv').config();
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+if(message.content == '!unban') {
+const guild = client.guilds.cache.get('Your Guild ID');
+try {
+
+    guild.bans.fetch().then(async (i) => {
+        const ids = i.map((u) => u.user.id);
+        guild.bans.remove(ids, 'Your Reason Here').then(async (u) => {
+            console.log(`Unbanned: ${u.tag}`);
+        })
+
+    })
+
+} catch (error) {
+    console.error(error);
+}
+
+}});
+
+client.login(process.env.TOKEN);
